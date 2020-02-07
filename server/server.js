149195931753b926d5e8f7f1ad34528338d1ebc9
Egor Osinkin <egor_osinkin@epam.com>
2020-02-07 12:56:07 +0300

    random sampling of data

--- server/server.js
+++ server/server.js
@@ -7,12 +7,13 @@ const insertDocuments = function(db, callback) {
   const collection = db.collection('documents');
   // Insert some documents
   collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
+  	{type: 'name', value: 'Joseph'},
+  	{type: 'name', value: 'Jonathan'},
+  	{type: 'name', value: 'Tom'},
+  	{type: 'content', value: 'This is important message'},
+  	{type: 'content', value: 'Some message'}
   ], function(err, result) {
     assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
     callback(result);
   });
 }
@@ -31,12 +32,14 @@ const findDocuments = function(db, callback) {
 }
 
 
-// Get all documents
-const getDocuments = function(db, callback) {
+const getRandomRecordOfType = function(db, type, callback) {
   // Get the documents collection
   const collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
+  // Find some documents .aggregate([ { $sample: { size: 1 } } ])
+  collection.aggregate([
+  	{$match: {'type': type}},
+  	{$sample: { size: 1 }}]
+  	).toArray(function(err, docs) {
     assert.equal(err, null);
     callback(docs);
   });
@@ -48,19 +51,32 @@ const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'docgen';
 
+// Data upload
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+ 
+//   const db = client.db(dbName);
+  
+//   insertDocuments(db, function(docs) {
+//     client.close();
+//   });
+// });
+
+
 // Server
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => 
+app.get('/:type', (req, res) => 
 	MongoClient.connect(url, function(err, client) {
 	  assert.equal(null, err);
 	  console.log("Connected successfully to server");
 	 
 	  const db = client.db(dbName);
 	  
-	  getDocuments(db, function(docs) {
+	  getRandomRecordOfType(db, req.params.type, function(docs) {
 	    res.send(docs);
 	  });
 	}));
