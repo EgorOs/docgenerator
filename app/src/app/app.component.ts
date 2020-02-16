cdb2832f7c22be5896ae4de9021d0f8ec3f1c922
Egor Osinkin <egoros97@yandex.ru>
2020-02-17 00:11:58 +0300

    simple shapes as labels

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +8,54 @@ import { Component} from '@angular/core';
 export class AppComponent {
   highlight = false
 
-  toggleLabels(event){
+  toggleLabels(event) {
     if (this.highlight) {
       this.removeHighlight();
       this.highlight = false;
-    }
-    else {
+    } else {
       this.highlightLabels();
       this.highlight = true;
     }
   }
 
+  hideText(element) {
+    element.style.color = 'rgba(0, 0, 0, 0)';
+    element.style.textShadow = 'none';
+  }
+
+  showText(element) {
+    element.style.color = 'rgba(0, 0, 0, 1)';
+    element.style.textShadow = '0px 0px 2px rgba(0, 0, 0, 0.5)';
+  }
+
   highlightLabels() {
     const content = document.getElementsByTagName("app-content")[0];
-    content.getElementsByTagNameNS("http://www.w3.org/1999/xhtml","p")[0].style.background = 'blue';
+    content.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "p")[0].style.background = 'blue';
+    this.hideText(content);
+
+    const docfrom = document.getElementsByTagName("app-doc-from")[0];
+    docfrom.style.background = 'green';
+    this.hideText(docfrom);
+
+    const contact = document.getElementsByTagName("app-contact")[0];
+    contact.style.background = 'hsl(180, 50%, 50%)';
+    this.hideText(contact);
+
+    const logo = document.getElementsByTagName("app-logo")[0];
+    logo.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "div")[0].style.background = 'hsl(120, 50%, 50%)';
   }
 
   removeHighlight() {
     const content = document.getElementsByTagName("app-content")[0];
-    content.getElementsByTagNameNS("http://www.w3.org/1999/xhtml","p")[0].style.background = 'none';
-  }
+    content.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "p")[0].style.background = 'none';
+    this.showText(content);
 
+    const docfrom = document.getElementsByTagName("app-doc-from")[0];
+    docfrom.style.background = 'none';
+    this.showText(docfrom);
+
+    const contact = document.getElementsByTagName("app-contact")[0];
+    contact.style.background = 'none';
+    this.showText(contact);
+  }
 }
