1a9616dd716bb4d8c2295fb0414cd773525e4912
Egor Osinkin <egor_osinkin@epam.com>
2020-03-06 19:26:47 +0300

    areas labels are generated

--- dataset/tags_based_dataset_generator.py
+++ dataset/tags_based_dataset_generator.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import csv
+import json
 from math import ceil
 
 from selenium import webdriver
@@ -12,7 +13,8 @@ class DocumentGenerator:
     def __init__(self, driver, dataset_path="dataset"):
         self.driver = driver
         self.data_path = Path(dataset_path) / "img"
-        self.tokens_coord = Path(dataset_path) / "coordinates"
+        self.coord_path = Path(dataset_path) / "coordinates"
+        self.areas_path = Path(dataset_path) / "areas"
 
     def _generate_single(
             self,
@@ -22,19 +24,28 @@ class DocumentGenerator:
             window_height,
             scale_factor,
     ):
+        """ 
+        Generates a single element of dataset, that includes image, CSV with coordinates of tokens,
+        JSON with coordinates of labelled areas
+        """
         self.driver.get(url)
         self.driver.set_window_size(
             window_width * scale_factor, window_height * scale_factor
         )
+
+        # Scale the page to get high resolution document
+        # works properly only for browser in headless mode
         self.driver.execute_script(f"document.body.style.zoom='{scale_factor * 100}%'")
         self.driver.save_screenshot(str(self.data_path / filename))
-
-        body_element = self.driver.find_element_by_tag_name("body")
         self._save_coordinates_to_file(f'coordinates_{filename.split(".")[0]}', scale_factor)
+
+        areas = ['body', 'reference-from', 'reference-to']
+        self._save_areas_to_json(f'areas_{filename.split(".")[0]}', areas, scale_factor)
 
     def _make_dirs(self):
         os.makedirs(str(self.data_path), exist_ok=True)
-        os.makedirs(str(self.tokens_coord), exist_ok=True)
+        os.makedirs(str(self.coord_path), exist_ok=True)
+        os.makedirs(str(self.areas_path), exist_ok=True)
 
     def describe_tokens(self, scale_factor):
         """
@@ -55,12 +66,40 @@ class DocumentGenerator:
         return list_transform
 
     def _save_coordinates_to_file(self, file_name, scale_factor):
-        """Saves coordinates to CSV file"""
+        """ Saves coordinates to CSV file """
         fieldnames = ['offset', 'word', 'x', 'y', 'width', 'height', 'label']
-        with open(self.tokens_coord / f'{file_name}.csv', "w", newline="") as result:
+        with open(self.coord_path / f'{file_name}.csv', "w", newline="") as result:
             writer = csv.writer(result, delimiter=',')
             writer.writerow(fieldnames)
             writer.writerows(self.describe_tokens(scale_factor))
+
+    def _save_areas_to_json(self, file_name, areas, scale_factor):
+        output = dict()
+        page_size = self.driver.get_window_size()
+        output['class'] = 'NO_CLASS'
+        output['pageSizes'] = [{
+            'pw': page_size['width'],
+            'ph': page_size['height'],
+        }]
+        tags = []
+        for area_class in areas:
+            elements = self.driver.find_elements_by_class_name(area_class)
+            for element in elements:
+                area_coords = {
+                    'p': 1,
+                    'x': ceil(element.location['x'] * scale_factor),
+                    'y': ceil(element.location['y'] * scale_factor),
+                    'width': ceil(element.size['width'] * scale_factor),
+                    'height': ceil(element.size['height'] * scale_factor)
+                }
+                tag_info = {
+                    'type': area_class,
+                    'coords': area_coords
+                }
+                tags.append(tag_info)
+        output['tags'] = tags
+        with open(self.areas_path / f'{file_name}.json', "w") as result:
+            result.write(json.dumps(output, indent=2))
 
     def generate(
             self,
@@ -98,4 +137,4 @@ if __name__ == "__main__":
 
     document_gen = DocumentGenerator(driver)
     with document_gen as dg:
-        dg.generate(10, window_width=width, window_height=height, scale_factor=1.2)
+        dg.generate(1, window_width=width, window_height=height, scale_factor=1.2)
